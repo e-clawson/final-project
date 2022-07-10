48dc12c1ca112e4d75707a5fd1d229a340865525
Elizabeth Clawson <emclawson1@gmail.com>
2022-07-10 21:55:39 +0530

    update UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+    skip_before_action :authorized!, only: [:create ]
+
+    def create 
+      user = User.create!(user_params)
+      session[:user_id] = user.id
+      render json: UserSerializer.new(user), status: :created
+    end
+  
+    def show #"/me"
+      render json: UserSerializer.new(@current_user), status: :ok
+    end
+  
+    private
+  
+    def user_params
+      params.permit(:username, :first_name, :last_name, :email, :password, :password_confirmation)
+    end
 end
