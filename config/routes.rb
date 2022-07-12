ab52a7455c89348bc90e6da1c7cbff3b33c1fcab
Elizabeth Clawson <emclawson1@gmail.com>
2022-07-12 13:02:43 +0530

    add user_serializer, add foreign key to items

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
-  resources :items
-  resources :users
+  scope :api do
+    scope :v1 do
+      get "/ordered-prompts", to: "propmts#ordered"
+      get "/most-stories", to: "posts#most_stories"
+      post "/auth/:provider/callback", to: "sessions#omniauth"
+
+      resources :users, only: [:update, :destroy]
+      post "/signup", to: "users#create"
+      get "/me", to: "users#show"
+      post "/login", to: "sessions#create"
+      delete "/logout", to: "sessions#destroy"
+      
+    
+      resources :items
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
