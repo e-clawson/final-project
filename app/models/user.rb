4e0e9900b41314e88c87b40b8533068fae9ea58e
Elizabeth Clawson <emclawson1@gmail.com>
2022-07-12 14:19:11 +0530

    update with notes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ApplicationRecord
     #associations
-    # has_many :stories
+    # has_many :items, dependent: :destroy - (will destroy the items that a user made if they delete their account)
     # has_many :prompts, through: :stories
 
     #password protection - password_digest
     has_secure_password
 
      # validations
+     #dont validate password presence because has secure password
+     #has presence validation and Oauth would get messed up if you did that
      validates :username, uniqueness: true, presence: true, length: {in: 4..25}
      validates :email, presence: true, uniqueness: true, format: {with: /\A(?<username>[^@\s]+)@((?<domain_name>[-a-z0-9]+)\.(?<domain>[a-z]{2,}))\z/i}
      validates :password, length: {in: 6..25}
