9fe9fcc85dbd94a10f2c5082353c6c9d3a78e66e
Elizabeth Clawson <emclawson1@gmail.com>
2022-07-12 14:15:30 +0530

    add validations and password protection to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+    #associations
+    # has_many :stories
+    # has_many :prompts, through: :stories
+
+    #password protection - password_digest
+    has_secure_password
+
+     # validations
+     validates :username, uniqueness: true, presence: true, length: {in: 4..25}
+     validates :email, presence: true, uniqueness: true, format: {with: /\A(?<username>[^@\s]+)@((?<domain_name>[-a-z0-9]+)\.(?<domain>[a-z]{2,}))\z/i}
+     validates :password, length: {in: 6..25}
 end
